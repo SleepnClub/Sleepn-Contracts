3a7eac3205c4e36f8ab2687c466858fb296998a8
mcc <60859013+AlexisBal@users.noreply.github.com>
2022-05-11 12:59:53 +0200

    master

--- Contracts/contracts/SleepToken.sol
+++ Contracts/contracts/SleepToken.sol
@@ -1,14 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract SleepToken is ERC20, ERC20Burnable, Pausable, Ownable {
-    constructor() ERC20("SleepToken", "SLP") {
-        _mint(msg.sender, 1000000000000000 * 10 ** decimals());
+contract SleepToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, OwnableUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("SleepToken", "SLP");
+        __ERC20Burnable_init();
+        __Pausable_init();
+        __Ownable_init();
+
+        _mint(msg.sender, 100000000000 * 10 ** decimals());
     }
 
     function pause() public onlyOwner {
