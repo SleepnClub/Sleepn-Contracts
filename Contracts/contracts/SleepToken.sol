80b056dcda22555f2e0581a2d9f024da2960ab08
mcc <60859013+AlexisBal@users.noreply.github.com>
2022-05-12 01:11:45 +0200

    master

--- Contracts/contracts/SleepToken.sol
+++ Contracts/contracts/SleepToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
@@ -8,6 +8,8 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract SleepToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, OwnableUpgradeable {
+    address public nftDexAddress;
+
     function initialize(uint256 _totalSupply) initializer public {
         __ERC20_init("SleepToken", "SLP");
         __ERC20Burnable_init();
@@ -16,18 +18,37 @@ contract SleepToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable
         _mint(address(this), _totalSupply * 10 ** decimals());
     }
 
+    // Access management for token management functions
+    modifier onlyDex(){
+        require(isInitialized(), "Dex address not configured!");
+        require(msg.sender == nftDexAddress, "Access forbidden");
+        _;
+    }
+
+    // Verifies that DEX address has been configured
+    function isInitialized() internal returns(bool) {
+        if (nftDexAddress != address(0)) {
+            return true;
+        }
+        return false;
+    }
+
+    // Stop the contract
     function pause() public onlyOwner {
         _pause();
     }
 
+    // Start the contract
     function unpause() public onlyOwner {
         _unpause();
     }
 
+    // Mint tokens for this smart contract
     function mint(uint256 _amount) public onlyOwner {
         _mint(address(this), _amount);
     }
 
+    // Burn tokens of this smart contract
     function burn(uint256 _amount) public override onlyOwner {
         _burn(address(this), _amount);
     }
@@ -39,4 +60,13 @@ contract SleepToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable
     {
         super._beforeTokenTransfer(from, to, amount);
     }
+
+    // NFT Investment : Buy, Upgrade
+    function investNft(address _owner, uint256 _amount) public onlyDex {
+        require(_amount > 0, "Incorrect amount");
+        uint256 allowance = allowance(_owner, msg.sender);
+        require(allowance >= _amount,"Check the token allowance")
+        _burn(_owner, _amount);
+    }
+
 }
