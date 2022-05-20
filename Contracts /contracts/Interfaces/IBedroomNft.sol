8d5c302b963f01639dc77bd5b922467d773bf863
mcc <60859013+AlexisBal@users.noreply.github.com>
2022-05-20 16:32:31 +0200

    master

--- Contracts /contracts/Interfaces/IBedroomNft.sol
+++ Contracts /contracts/Interfaces/IBedroomNft.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
 
+import "./IUpgradeNft.sol";
+
 /// @title Interface of the BedroomNft Contract
 /// @author Alexis Balayre
 /// @notice Contains the external functions of the BedroomNft Contract
