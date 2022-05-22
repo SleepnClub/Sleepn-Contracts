083fa0c6f7b573cce0d7e6541d0ca1fb33fef6b4
mcc <60859013+AlexisBal@users.noreply.github.com>
2022-05-22 02:00:26 +0200

    master

--- Contracts /scripts/deployUpgradeNft.ts
+++ Contracts /scripts/deployUpgradeNft.ts
@@ -1,29 +1,17 @@
-import { ethers, upgrades } from 'hardhat';
+import { ethers } from 'hardhat';
 
 async function main() {
   // Parameters
   const subscriptionId = 33;
   const vrfCoordinator = "0xAE975071Be8F8eE67addBC1A82488F1C24858067";
-  const link_token_contract = "0xb0897686c545045aFc77CF20eC7A532E3120E0F1";
   const keyHash = "0xd729dc84e21ae57ffb6be0053bf2b0668aa2aaf300a2a7b2ddf7dc0bb6e875a8";
-  const bedroomNftContract = "0xb150a58d376DeF437AB8b19ab351db7BA2C1eDEe";
   
   // Deployment
   const UpgradeNft = await ethers.getContractFactory('UpgradeNft');
   console.log('Deploying UpgradeNft contract...');
-  const upgradeNft = await upgrades.deployProxy(
-    UpgradeNft, 
-    [
-      subscriptionId, 
-      vrfCoordinator, 
-      link_token_contract, 
-      keyHash,
-      bedroomNftContract
-    ], 
-    { initializer: "initialize"}
-  );
+  const upgradeNft = await UpgradeNft.deploy(subscriptionId, vrfCoordinator, keyHash);
   await upgradeNft.deployed();
-  console.log('UpgradeNft contract Proxy deployed to:', upgradeNft.address);
+  console.log('UpgradeNft contract deployed to:', upgradeNft.address);
 }
 
 main().catch((error) => {
--- Contracts /scripts/upgradeDex.ts
+++ Contracts /scripts/upgradeDex.ts
@@ -2,7 +2,7 @@ import { ethers, upgrades } from 'hardhat';
 
 async function main() {
     // Parameters
-    const addressProxy = "0x4FC7D46Ebc6165A72926d0711f0855f14bAeaAd7";
+    const addressProxy = "0x3240E10ad3EBc6b66E8FaAA0E288123702B3A29f";
 
     // Upgrades
     const DexV2 = await ethers.getContractFactory('Dex');
