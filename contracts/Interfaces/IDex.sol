2506b3b19471ebfa39d9dbd718d3b1435767a12e
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2022-07-03 08:53:11 +0200

    Update IDex.sol

--- contracts/Interfaces/IDex.sol
+++ contracts/Interfaces/IDex.sol
@@ -1,86 +1,68 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.15;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "./IBedroomNft.sol";
 import "./ISleepToken.sol";
 import "./IUpgradeNft.sol";
 
 /// @title Interface of GetSleepn Decentralized Exchange Contract
-/// @author Alexis Balayre
+/// @author Sleepn
 /// @notice This contract can be use to mint and upgrade a Bedroom NFT
 interface IDex {
-    /// @notice Informations about an upgrade
-    struct upgradeInfos {
-        uint256 indexAttribute;
-        uint256 valueToAddMax;
-        uint256 price;
+    /// @notice Upgrade costs
+    struct Upgrade {
+        bool isOwned; // is Owned
+        uint256 price; // Upgrade Cost
     }
 
-    /// @notice Purchase cost and Upgrade costs
-    struct NftPrices {
-        uint256 purchaseCost; // Initial Cost
-        mapping(uint256 => upgradeInfos) upgradeCosts; // Upgrade Costs
-    }
-
-    /// @notice Received Money Event
-    event ReceivedMoney(address indexed sender, uint256 price);
-
-    /// @notice Buy NFT Event
-    event BuyNft(
+    /// @notice Buy Bedroom NFT Event
+    event BuyBedroomNft(
         address indexed owner,
-        IBedroomNft.Category category,
-        uint256 designId
+        uint256 designId,
+        uint256 price
     );
 
-    /// @notice Upgrade NFT Event
-    event UpgradeNft(
+    /// @notice Buy Upgrade NFT Event
+    event BuyUpgradeNft(
         address indexed owner,
-        IBedroomNft.Category category,
-        uint256 tokenId,
-        uint256 newDesignId,
-        uint256 upgradeDesignId,
-        uint256 upgradeIndex,
+        uint256 upgradeNftId,
         uint256 price
     );
 
     /// @notice Withdraw Money Event
     event WithdrawMoney(address indexed receiver, uint256 price);
 
-    /// @notice Settles Team Wallet contract address
-    /// @param _newAddress New Team Wallet address
-    /// @dev This function can only be called by the owner of the contract
-    function setTeamWallet(address _newAddress) external;
-
     /// @notice Settles contracts addresses
     /// @param _sleepToken Address of the Sleep Token contract
     /// @param _bedroomNft Address of the Bedroom NFT contract
     /// @param _upgradeNft Address of the Upgrade NFT contract
+    /// @param _teamWallet New Team Wallet address
+    /// @param _devWallet New Dev Wallet address
+    /// @param _tokenAddress New Payment Token contract address
     /// @dev This function can only be called by the owner of the contract
-    function setContracts(
+    function setAddresses(
         ISleepToken _sleepToken,
         IBedroomNft _bedroomNft,
-        IUpgradeNft _upgradeNft
+        IUpgradeNft _upgradeNft,
+        address _teamWallet,
+        address _devWallet,
+        IERC20 _tokenAddress
     ) external;
 
-    /// @notice Settles NFTs purchase prices
-    /// @param _category Category of the NFT
+    /// @notice Settles NFTs purchase price
     /// @param _price Purchase price of the NFT
     /// @dev This function can only be called by the owner of the contract
-    function setBuyingPrices(IBedroomNft.Category _category, uint256 _price)
+    function setBuyingPrice(uint256 _price)
         external;
 
-    /// @notice Settles NFTs upgrade prices
-    /// @param _category Category of the Bedroom NFT
-    /// @param _upgradeIndex Index of the upgrade
-    /// @param _indexAttribute Index of the attribute concerned by the upgrade
-    /// @param _valueToAddMax Value max to add to the existing score
+    /// @notice Settles NFTs upgrade price
+    /// @param _upgradeId Id of the upgrade
     /// @param _price Purchase price of the Upgrade NFT
     /// @dev This function can only be called by the owner of the contract
-    function setUpgradePrices(
-        IBedroomNft.Category _category,
-        uint256 _upgradeIndex,
-        uint256 _indexAttribute,
-        uint256 _valueToAddMax,
+    function setUpgradePrice(
+        uint256 _upgradeId,
         uint256 _price
     ) external;
 
@@ -88,34 +70,40 @@ interface IDex {
     /// @dev This function can only be called by the owner or the dev Wallet
     function withdrawMoney() external;
 
-    /// @notice Returns the balance of the contract
-    /// @return _balance Balance of the contract
-    function getBalance() external view returns (uint256);
-
-    /// @notice Launches the mint procedure of a Bedroom NFT
-    /// @param _category Category of the desired Bedroom NFT
-    /// @param _designId Design Id of the NFT
-    function buyNft(IBedroomNft.Category _category, uint256 _designId)
-        external
-        payable;
-
-    /// @notice Launches the mint procedure of an Upgrade NFT
-    /// @notice Update NFT minting will automatically update the Bedroom NFT
-    /// @param _tokenId Category of the desired Bedroom NFT
-    /// @param _newDesignId Design Id of the NFT
-    /// @param _upgradeDesignId Category of the desired Bedroom NFT
-    /// @param _upgradeIndex Index of the upgrade
-    /// @param _price Price of the NFT
-    function upgradeNft(
-        uint256 _tokenId,
-        uint256 _newDesignId,
-        uint256 _upgradeDesignId,
-        uint256 _upgradeIndex,
-        uint256 _price
+    /// @notice Settles NFTs upgrade prices
+    /// @param _upgradeIds Id of the upgrade
+    /// @param _prices Purchase price of the Upgrade NFT
+    /// @dev This function can only be called by the owner or the dev Wallet
+    function setUpgradePriceBatch(
+        uint256[] memory _upgradeIds,
+        uint256[] memory _prices
     ) external;
 
-    /// @notice Settles Dev Wallet address
-    /// @param _devWallet New Dev Wallet address
-    /// @dev This function can only be called by the owner of the contract
-    function setDevAddress(address _devWallet) external;
+    /// @notice Returns the price of an Upgrade Nft
+    /// @param _id Id of the upgrade
+    /// @return _price Price of the Upgrade Nft
+    function getUpgradePrice(
+        uint256 _id
+    ) external view returns (uint256 _price);
+
+    /// @notice Returns the balance of the contract
+    /// @return _balance Balance of the contract
+    function getBalance() external view returns (uint256 _balance);
+
+    /// @notice Buy an Upgrade Nft
+    /// @param _upgradeNftId Id of the Upgrade NFT
+    function buyUpgradeNft(
+        uint256 _upgradeNftId
+    ) external;
+
+    /// @notice Buy an Upgrade Nft
+    /// @param _upgradeNftId Id of the Upgrade NFT
+    /// @param _bedroomNftId Id of the Bedroom NFT
+    /// @param _newDesignId New Design Id of the Bedroom NFT
+    function linkUpgradeNft(
+        uint256 _upgradeNftId, 
+        uint256 _bedroomNftId, 
+        uint256 _newDesignId
+    ) external;
+
 }
