7c18f1d7e361c5b6584bf1f76ce1cbd4ce046a81
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2022-07-03 08:16:13 +0200

    Update Dex.sol

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import "./Interfaces/ISleepToken.sol";
-import "./Interfaces/IBedroomNft.sol";
-import "./Interfaces/IUpgradeNft.sol";
+import "../Interfaces/ISleepToken.sol";
+import "../Interfaces/IBedroomNft.sol";
+import "../Interfaces/IUpgradeNft.sol";
 
 /// @title GetSleepn Decentralized Exchange Contract
-/// @author Alexis Balayre
+/// @author Sleepn
 /// @notice This contract can be use to mint and upgrade a Bedroom NFT
 contract Dex is Initializable, OwnableUpgradeable {
     /// @notice Dex Contract address
@@ -25,21 +25,17 @@ contract Dex is Initializable, OwnableUpgradeable {
     /// @notice UpgradeNFT Contract
     IUpgradeNft public upgradeNftInstance;
 
-    /// @notice Informations about an upgrade
-    struct upgradeInfos {
-        uint256 indexAttribute;
-        uint256 valueToAddMax;
-        uint256 price;
+    /// @notice Purchase cost and Upgrade cost
+    uint256 public purchaseCost;
+
+    /// @notice Upgrade costs
+    struct Upgrade {
+        bool isOwned; // is Owned
+        uint256 price; // Upgrade Cost
     }
 
-    /// @notice Purchase cost and Upgrade costs
-    struct NftPrices {
-        uint256 purchaseCost; // Initial Cost
-        mapping(uint256 => upgradeInfos) upgradeCosts; // Upgrade Costs
-    }
-
-    /// @notice Purchase cost and Upgrade costs depending on the category of the NFT
-    mapping(IBedroomNft.Category => NftPrices) public prices;
+    /// @notice Upgrade costs depending on the id of the Upgrade Nft
+    mapping(uint256 => Upgrade) public upgradeCosts;
 
     /// @dev Dev Wallet
     address private devWallet;
@@ -47,24 +43,17 @@ contract Dex is Initializable, OwnableUpgradeable {
     /// @notice Payment Token
     IERC20 public paymentToken;
 
-    /// @notice Received Money Event
-    event ReceivedMoney(address indexed sender, uint256 price);
-
-    /// @notice Buy NFT Event
-    event BuyNft(
+    /// @notice Buy Bedroom NFT Event
+    event BuyBedroomNft(
         address indexed owner,
-        IBedroomNft.Category category,
-        uint256 designId
+        uint256 designId,
+        uint256 price
     );
 
-    /// @notice Upgrade NFT Event
-    event UpgradeNft(
+    /// @notice Buy Upgrade NFT Event
+    event BuyUpgradeNft(
         address indexed owner,
-        IBedroomNft.Category category,
-        uint256 tokenId,
-        uint256 newDesignId,
-        uint256 upgradeDesignId,
-        uint256 upgradeIndex,
+        uint256 upgradeNftId,
         uint256 price
     );
 
@@ -94,58 +83,78 @@ contract Dex is Initializable, OwnableUpgradeable {
         assert(address(sleepTokenInstance) != address(0));
     }
 
-    /// @notice Settles Team Wallet contract address
-    /// @param _newAddress New Team Wallet address
-    /// @dev This function can only be called by the owner of the contract
-    function setTeamWallet(address _newAddress) external onlyOwner {
-        teamWallet = _newAddress;
-    }
-
     /// @notice Settles contracts addresses
     /// @param _sleepToken Address of the Sleep Token contract
     /// @param _bedroomNft Address of the Bedroom NFT contract
     /// @param _upgradeNft Address of the Upgrade NFT contract
+    /// @param _teamWallet New Team Wallet address
+    /// @param _devWallet New Dev Wallet address
+    /// @param _tokenAddress New Payment Token contract address
     /// @dev This function can only be called by the owner of the contract
-    function setContracts(
+    function setAddresses(
         ISleepToken _sleepToken,
         IBedroomNft _bedroomNft,
-        IUpgradeNft _upgradeNft
+        IUpgradeNft _upgradeNft,
+        address _teamWallet,
+        address _devWallet,
+        IERC20 _tokenAddress
     ) external onlyOwner {
         sleepTokenInstance = _sleepToken;
         bedroomNftInstance = _bedroomNft;
         upgradeNftInstance = _upgradeNft;
+        teamWallet = _teamWallet;
+        devWallet = _devWallet;
+        paymentToken = _tokenAddress;
     }
 
-    /// @notice Settles NFTs purchase prices
-    /// @param _category Category of the NFT
+    /// @notice Settles NFTs purchase price
     /// @param _price Purchase price of the NFT
     /// @dev This function can only be called by the owner of the contract
-    function setBuyingPrices(IBedroomNft.Category _category, uint256 _price)
+    function setBuyingPrice(uint256 _price)
         external
         onlyOwner
     {
-        prices[_category].purchaseCost = _price;
+        purchaseCost = _price;
+    }
+
+    /// @notice Settles NFTs upgrade price
+    /// @param _upgradeId Id of the upgrade
+    /// @param _price Purchase price of the Upgrade NFT
+    /// @dev This function can only be called by the owner of the contract
+    function setUpgradePrice(
+        uint256 _upgradeId,
+        uint256 _price
+    ) external onlyOwner {
+        upgradeCosts[ _upgradeId].price = _price;
     }
 
     /// @notice Settles NFTs upgrade prices
-    /// @param _category Category of the Bedroom NFT
-    /// @param _upgradeIndex Index of the upgrade
-    /// @param _indexAttribute Index of the attribute concerned by the upgrade
-    /// @param _valueToAddMax Value max to add to the existing score
-    /// @param _price Purchase price of the Upgrade NFT
-    /// @dev This function can only be called by the owner of the contract
-    function setUpgradePrices(
-        IBedroomNft.Category _category,
-        uint256 _upgradeIndex,
-        uint256 _indexAttribute,
-        uint256 _valueToAddMax,
-        uint256 _price
-    ) external onlyOwner {
-        prices[_category].upgradeCosts[_upgradeIndex] = upgradeInfos(
-            _indexAttribute,
-            _valueToAddMax,
-            _price
+    /// @param _upgradeIds Id of the upgrade
+    /// @param _prices Purchase price of the Upgrade NFT
+    /// @dev This function can only be called by the owner or the dev Wallet
+    function setUpgradePriceBatch(
+        uint256[] memory _upgradeIds,
+        uint256[] memory _prices
+    ) external {
+        require(
+            msg.sender == owner() || msg.sender == devWallet,
+            "Access Forbidden"
         );
+        require(_upgradeIds.length == _prices.length, "ERC1155: _upgradeIds and _prices length mismatch");
+        for(uint256 i = 0; i < _upgradeIds.length; i++) {
+            uint256 upgradeId = _upgradeIds[i];
+            uint256 price =  _prices[i];
+            upgradeCosts[upgradeId].price = price;
+        }
+    }
+
+    /// @notice Returns the price of an Upgrade Nft
+    /// @param _id Id of the upgrade
+    /// @return _price Price of the Upgrade Nft
+    function getUpgradePrice(
+        uint256 _id
+    ) external view returns (uint256) {
+        return upgradeCosts[_id].price;
     }
 
     /// @notice Withdraws the money from the contract
@@ -167,62 +176,37 @@ contract Dex is Initializable, OwnableUpgradeable {
     }
 
     /// @notice Launches the mint procedure of a Bedroom NFT
-    /// @param _category Category of the desired Bedroom NFT
     /// @param _designId Design Id of the NFT
-    function buyNft(IBedroomNft.Category _category, uint256 _designId)
-        public
+    function buyBedroomNft(uint256 _designId)
+        external
     {
-        // Price of the Bedroom NFT
-        uint256 price = prices[_category].purchaseCost;
-
         // Check Balance of sender
-        require(paymentToken.balanceOf(msg.sender) >= price * 1e17, "Not enough funds");
+        require(paymentToken.balanceOf(msg.sender) >= purchaseCost * 1e17, "Not enough funds");
 
         // Check Allowance - tx to approve
-        require(paymentToken.allowance(msg.sender, address(this)) >= price * 1e17, "Check allowance");
+        require(paymentToken.allowance(msg.sender, address(this)) >= purchaseCost * 1e17, "Check allowance");
 
         // Token Transfer
-        paymentToken.transferFrom(msg.sender, address(this), price * 1e17);
+        paymentToken.transferFrom(msg.sender, address(this), purchaseCost * 1e17);
 
         // NFT Minting
         bedroomNftInstance.mintingBedroomNft(
             _designId,
-            price,
-            _category,
             msg.sender
         );
-        emit BuyNft(msg.sender, _category, _designId);
-        emit ReceivedMoney(msg.sender, price);
+        emit BuyBedroomNft(msg.sender, _designId, purchaseCost);
     }
 
-    /// @notice Launches the mint procedure of an Upgrade NFT
-    /// @notice Update NFT minting will automatically update the Bedroom NFT
-    /// @param _tokenId Category of the desired Bedroom NFT
-    /// @param _newDesignId Design Id of the NFT
-    /// @param _upgradeDesignId Category of the desired Bedroom NFT
-    /// @param _upgradeIndex Index of the upgrade
-    function upgradeNft(
-        uint256 _tokenId,
-        uint256 _newDesignId,
-        uint256 _upgradeDesignId,
-        uint256 _upgradeIndex
+    /// @notice Buy an Upgrade Nft
+    /// @param _upgradeNftId Id of the Upgrade NFT
+    function buyUpgradeNft(
+        uint256 _upgradeNftId
     ) external {
-        // Get NFT informations
-        IBedroomNft.NftOwnership memory nftOwnership = bedroomNftInstance
-            .getNftOwnership(_tokenId);
-        IBedroomNft.Category category = nftOwnership.category;
+        // Availability of Nft
+        require(upgradeCosts[_upgradeNftId].isOwned == false, "Nft not available");
 
-        // Get Upgrade infos
-        uint256 price = prices[category].upgradeCosts[_upgradeIndex].price;
-        uint256 value = prices[category]
-            .upgradeCosts[_upgradeIndex]
-            .valueToAddMax;
-        uint256 index = prices[category]
-            .upgradeCosts[_upgradeIndex]
-            .indexAttribute;
-
-        // Sender is owner
-        require(msg.sender == nftOwnership.owner, "Wrong sender");
+        // Price 
+        uint256 price = upgradeCosts[_upgradeNftId].price;
 
         // Check Balance of sender
         require(sleepTokenInstance.balanceOf(msg.sender) >= price * 1e18, "Not enough funds");
@@ -236,58 +220,43 @@ contract Dex is Initializable, OwnableUpgradeable {
         // Burns tokens
         sleepTokenInstance.burnFrom(msg.sender, price * 1e18);
 
-        // Minting of the upgrade token
-        upgradeNftInstance.mintingUpgradeNft(
-            _tokenId,
-            _newDesignId,
-            _upgradeDesignId,
-            price,
-            index,
-            value,
-            msg.sender
-        );
-        emit UpgradeNft(
+        // Transfer ownership 
+        upgradeNftInstance.transferUpgradeNft(_upgradeNftId, msg.sender);
+        emit BuyUpgradeNft(
             msg.sender,
-            category,
-            _tokenId,
-            _newDesignId,
-            _upgradeDesignId,
-            _upgradeIndex,
+            _upgradeNftId,
             price
         );
     }
 
-    /// @notice Settles Dev Wallet address
-    /// @param _devWallet New Dev Wallet address
-    /// @dev This function can only be called by the owner of the contract
-    function setDevAddress(address _devWallet) external onlyOwner {
-        devWallet = _devWallet;
-    }
-
-    /// @notice Settles Payment Token contract address
-    /// @param _tokenAddress New Payment Token contract address
-    /// @dev This function can only be called by the owner of the contract
-    function setPaymentToken(IERC20 _tokenAddress) external onlyOwner { 
-        paymentToken = _tokenAddress;
+    /// @notice Buy an Upgrade Nft
+    /// @param _upgradeNftId Id of the Upgrade NFT
+    /// @param _bedroomNftId Id of the Bedroom NFT
+    /// @param _newDesignId New Design Id of the Bedroom NFT
+    function linkUpgradeNft(
+        uint256 _upgradeNftId, 
+        uint256 _bedroomNftId, 
+        uint256 _newDesignId
+    ) external {
+        upgradeNftInstance.linkUpgradeNft(
+            _upgradeNftId,
+            _bedroomNftId,
+            _newDesignId,
+            msg.sender
+        );
     }
 
     /// @notice Airdrops some Bedroom NFTs
     /// @param _addresses Addresses of the receivers 
-    /// @param _category Category of the Bedroom NFTs
     /// @param _designId Design of the Bedroom NFTs
-    /// @param _price Price of the NFTs
     /// @dev This function can only be called by the owner of the contract
     function airdropBedroomNFT(
         address[] memory _addresses, 
-        IBedroomNft.Category _category,
-        uint256 _designId, 
-        uint256 _price
+        uint256 _designId
     ) external onlyOwner {
-        for(uint i=0; i<_addresses.length; i++) {
+        for(uint256 i=0; i<_addresses.length; i++) {
             bedroomNftInstance.mintingBedroomNft(
                 _designId,
-                _price,
-                _category,
                 _addresses[i]
             );
         }
